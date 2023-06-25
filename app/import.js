b1023e1b17441482fb7794af835d41ef77c41239
Cameron Burt <silkenmeerkat@gmail.com>
2023-06-24 22:55:02 -0400

    Adding the project plan and some skeletons

---
+++ app/import.js
@@ -0,0 +1,12 @@
+//Api calls to go to https://github.com/foundryvtt/pf2e/tree/master/packs/equipment, search through the dir, and get all .json files. Ingest their data to DB?
+
+
+//Get list of files
+
+
+
+//Import File
+
+
+
+//
