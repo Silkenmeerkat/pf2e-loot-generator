192e5424f787b7bf9137ad324de4fa99639c9407
Cameron Burt <silkenmeerkat@mgmail.com>
2023-06-26 20:05:44 -0400

    pnpm

--- app/import.js
+++ app/import.js
@@ -1,6 +1,11 @@
 //Api calls to go to https://github.com/foundryvtt/pf2e/tree/master/packs/equipment, search through the dir, and get all .json files. Ingest their data to DB?
 
 
+async function getList(){
+    fetch 
+}
+
+
 //Get list of files
 
 
